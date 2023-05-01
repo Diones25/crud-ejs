fc03278e4967ddc0cc821ffde92ff22acb4d71d7
Diones <pereiradiones987@gmail.com>
2023-05-01 17:24:02 -0300

    update project

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import router from './routes/routes.js';
-import session from 'express-session';
-import flash from 'connect-flash';
 
 const app = express(); 
 
@@ -24,19 +22,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //App usar rotas
 app.use(router);
-
-//Configurar flash messages
-app.use(session({
-    secret:'flashblog',
-    saveUninitialized: true,
-    resave: true
-}));
-  
-app.use(flash());
-
-app.use(function(req, res, next){
-    res.locals.message = req.flash();
-    next();
-});
 
 export default app;   
