eb9785319687f23dc545496937159b4f900f7d68
Diones <pereiradiones987@gmail.com>
2023-05-12 22:10:48 -0300

    adicionando conexão dinâmica

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import dotev from 'dotenv';
 import chalk from 'chalk';
 import app from "./app.js";
+import conn from './db/conn.js'
 
 dotev.config();
 
-app.listen(process.env.PORT, () => {
-  console.log(chalk.bgGreenBright.black(`Servidor rodando: http://localhost:${process.env.PORT}`));
+conn
+  //.sync({ force: true })
+  .sync()
+  .then(() => {
+
+  app.listen(process.env.PORT, () => {
+    console.log(chalk.bgGreenBright.black(`Servidor rodando: http://localhost:${process.env.PORT}`));
+  });
+
+}).catch((err) => {
+  console.log(chalk.bgRedBright.blackBright(`ERRO: ${err}`));
 });
